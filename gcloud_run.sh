cc980866f0606509f023501728ce44e98f8319ba
GuyTevet <guy.tvt@gmail.com>
2018-07-03 21:36:55 +0300

    fix

--- gcloud_run.sh
+++ gcloud_run.sh
@@ -1,4 +1,4 @@
-python curriculum_training.py --ARCH=class_conditioned --GENERATOR_MODEL=Generator_GRU_class_conditioned --DISCRIMINATOR_MODEL=Discriminator_GRU_class_conditioned
+python3 curriculum_training.py --ARCH=class_conditioned --GENERATOR_MODEL=Generator_GRU_class_conditioned --DISCRIMINATOR_MODEL=Discriminator_GRU_class_conditioned
 
 sudo shutdown
 
